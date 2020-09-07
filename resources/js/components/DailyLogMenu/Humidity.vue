61183fe3caa5b901e136a3c4dab39d8e8a0dc276
Tri Edy <49435622+EdyW98@users.noreply.github.com>
2020-09-07 17:07:27 +0700

    minor change(1)

--- resources/js/components/DailyLogMenu/Humidity.vue
+++ resources/js/components/DailyLogMenu/Humidity.vue
@@ -1,57 +1,81 @@
 <template>
-    <v-app id="inspire">
-        <v-container 
-            fluid
-        >
-            <v-row
-                class="mx-0"
-                align="start"
-                justify="center"
-            >                  
-                <v-col class="text-center">
-                    <v-card
-                        class="px-2 mx-auto"
-                        width="fill"
-                        max-height="750"
-                        min-height="500"
-                        outlined
-                    >
-                        <v-card-title>Graf Kelembapan Udara</v-card-title>
-                        <apexchart height="100%" type="line" :options="optionsTempt" :series="seriesTempt"></apexchart>
-                    </v-card>
-                </v-col>
-            </v-row>
-            <v-spacer></v-spacer>
-            <v-row
-                align="start"
-                justify="center"
-            >                  
-                <v-col class="text-center">
-                    <v-card
-                        class="px-auto mx-auto"
-                        width="fill"
-                        outlined
-                    >
-                        <v-card-title>Tabel Kelembapan Udara</v-card-title>
-                        <v-data-table
-                            :headers="headers"
-                            :items="data"
-                            :page.sync="page"
-                            :items-per-page="15"
-                            :search="search"
-                            hide-default-footer
-                            class="elevation-1"
-                            @page-count="pageCount = $event"
-                        >
-                        </v-data-table>
-                        <div class="text-center pt-2">
-                            <v-pagination v-model="page" :length="pageCount" :total-visible="7"></v-pagination>
-                        </div>
-                    </v-card>
-                </v-col>
-            </v-row>
-        </v-container>
-    </v-app>
+  <v-app id="inspire">
+    <v-container 
+      fluid
+    >
+      <v-row
+        class="mx-0"
+        align="start"
+        justify="center"
+      >                  
+        <v-col class="text-center">
+          <v-card
+            class="px-2 mx-auto"
+            width="fill"
+            max-height="750"
+            min-height="500"
+            outlined
+          >
+          <v-card-title>Graf Temperatur</v-card-title>
+            <apexchart height="100%" type="line" :options="optionsHumidity" :series="seriesHumidity"></apexchart>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-spacer></v-spacer>
+      <v-row
+        align="start"
+        justify="center"
+      >                  
+        <v-col class="text-center">
+          <v-menu
+            ref="menu"
+            v-model="menu"
+            :close-on-content-click="false"
+            transition="scale-transition"
+            offset-y
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="search"
+              label="Cari"
+              prepend-icon="mdi-calendar"
+              readonly
+              v-bind="attrs"
+              v-on="on"
+            ></v-text-field>
+            </template>
+            <v-date-picker v-model="date" no-title scrollable>
+              <v-spacer></v-spacer>
+              <v-btn text color="primary" @click="resetSearchTable">Reset</v-btn>
+              <v-btn text color="primary" @click="setSearchTable">Set</v-btn>
+            </v-date-picker>
+          </v-menu>
+          <v-card
+            class="px-auto mx-auto"
+            width="fill"
+            outlined
+          >
+            <v-card-title>Tabel Temperature</v-card-title>
+            <v-data-table
+              :headers="headers"
+              :items="data"
+              :page.sync="page"
+              :items-per-page="15"
+              :search="search"
+              hide-default-footer
+              class="elevation-1"
+              @page-count="pageCount = $event"
+            >
+            </v-data-table>
+            <div class="text-center pt-2">
+              <v-pagination v-model="page" :length="pageCount" :total-visible="7"></v-pagination>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
@@ -64,17 +88,20 @@
       page: 1,
       pageCount: 0,
       search: '',
+      date: new Date().toISOString().substr(0, 10),
+      menu: false,
       
       //Tabel Kelembapan Udara
       headers: [        
           { text: 'Rata Rata', value: 'AvgHumidity', sortable: false },
           { text: 'Tertinggi', value: 'MaxHumidity', sortable: false },
           { text: 'Terendah', value: 'MinHumidity', sortable: false },
+          { text: 'Tanggal', value: 'created_at', sortable: false },
       ],
       data: [],
 
       //Chart Kelembapan Udara
-      seriesTempt: [
+      seriesHumidity: [
         {
               name: "Rata-Rata Kelembapan Udara",
               data: []
@@ -88,7 +115,7 @@
               data: []
             }
       ],
-      optionsTempt: {
+      optionsHumidity: {
         chart: {
           id: 'vuechart-example',
           toolbar: {
@@ -136,7 +163,7 @@
             data3.push(element.MinHumidity);
           });
 
-          this.seriesTempt = [
+          this.seriesHumidity = [
             {
               data: data1
             },
@@ -147,12 +174,22 @@
               data: data3
             },
           ];
-          this.optionsTempt = {
+          this.optionsHumidity = {
             xaxis: {
               categories: cat
             }
           }
         })
+      },
+
+      setSearchTable(){
+        this.search = this.date;
+        this.menu = false;
+      },
+
+      resetSearchTable(){
+        this.search = '';
+        this.menu = false;
       },
     },
 
--- resources/js/components/DailyLogMenu/Temperature.vue
+++ resources/js/components/DailyLogMenu/Temperature.vue
@@ -1,57 +1,81 @@
 <template>
-    <v-app id="inspire">
-        <v-container 
-            fluid
-        >
-            <v-row
-                class="mx-0"
-                align="start"
-                justify="center"
-            >                  
-                <v-col class="text-center">
-                    <v-card
-                        class="px-2 mx-auto"
-                        width="fill"
-                        max-height="750"
-                        min-height="500"
-                        outlined
-                    >
-                        <v-card-title>Graf Temperatur</v-card-title>
-                        <apexchart height="100%" type="line" :options="optionsTempt" :series="seriesTempt"></apexchart>
-                    </v-card>
-                </v-col>
-            </v-row>
-            <v-spacer></v-spacer>
-            <v-row
-                align="start"
-                justify="center"
-            >                  
-                <v-col class="text-center">
-                    <v-card
-                        class="px-auto mx-auto"
-                        width="fill"
-                        outlined
-                    >
-                        <v-card-title>Tabel Temperature</v-card-title>
-                        <v-data-table
-                            :headers="headers"
-                            :items="data"
-                            :page.sync="page"
-                            :items-per-page="15"
-                            :search="search"
-                            hide-default-footer
-                            class="elevation-1"
-                            @page-count="pageCount = $event"
-                        >
-                        </v-data-table>
-                        <div class="text-center pt-2">
-                            <v-pagination v-model="page" :length="pageCount" :total-visible="7"></v-pagination>
-                        </div>
-                    </v-card>
-                </v-col>
-            </v-row>
-        </v-container>
-    </v-app>
+  <v-app id="inspire">
+    <v-container 
+      fluid
+    >
+      <v-row
+        class="mx-0"
+        align="start"
+        justify="center"
+      >                  
+        <v-col class="text-center">
+          <v-card
+            class="px-2 mx-auto"
+            width="fill"
+            max-height="750"
+            min-height="500"
+            outlined
+          >
+          <v-card-title>Graf Temperatur</v-card-title>
+            <apexchart height="100%" type="line" :options="optionsTempt" :series="seriesTempt"></apexchart>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-spacer></v-spacer>
+      <v-row
+        align="start"
+        justify="center"
+      >                  
+        <v-col class="text-center">
+          <v-menu
+            ref="menu"
+            v-model="menu"
+            :close-on-content-click="false"
+            transition="scale-transition"
+            offset-y
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="search"
+              label="Cari"
+              prepend-icon="mdi-calendar"
+              readonly
+              v-bind="attrs"
+              v-on="on"
+            ></v-text-field>
+            </template>
+            <v-date-picker v-model="date" no-title scrollable>
+              <v-spacer></v-spacer>
+              <v-btn text color="primary" @click="resetSearchTable">Reset</v-btn>
+              <v-btn text color="primary" @click="setSearchTable">Set</v-btn>
+            </v-date-picker>
+          </v-menu>
+          <v-card
+            class="px-auto mx-auto"
+            width="fill"
+            outlined
+          >
+            <v-card-title>Tabel Temperature</v-card-title>
+            <v-data-table
+              :headers="headers"
+              :items="data"
+              :page.sync="page"
+              :items-per-page="15"
+              :search="search"
+              hide-default-footer
+              class="elevation-1"
+              @page-count="pageCount = $event"
+            >
+            </v-data-table>
+            <div class="text-center pt-2">
+              <v-pagination v-model="page" :length="pageCount" :total-visible="7"></v-pagination>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
@@ -64,12 +88,15 @@
       page: 1,
       pageCount: 0,
       search: '',
+      date: new Date().toISOString().substr(0, 10),
+      menu: false,
       
       //Tabel Temperature
       headers: [        
           { text: 'Rata Rata', value: 'AvgTemperature', sortable: false },
           { text: 'Maksimal', value: 'MaxTemperature', sortable: false },
           { text: 'Minimal', value: 'MinTemperature', sortable: false },
+          { text: 'Tanggal', value: 'created_at', sortable: false },
       ],
       data: [],
 
@@ -154,6 +181,16 @@
             }
           }
         })
+      },
+
+      setSearchTable(){
+        this.search = this.date;
+        this.menu = false;
+      },
+
+      resetSearchTable(){
+        this.search = '';
+        this.menu = false;
       },
     },
 
